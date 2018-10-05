5c19e1717c95bc9fde357756b91c96f3eb229966
ashllay <edu-01@live.com>
2018-10-05 17:23:34 -0300

    Ping Test

---
+++ PingTest/Main.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace PingTest
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Txb_IP = new System.Windows.Forms.TextBox();
+            this.Txb_Port = new System.Windows.Forms.TextBox();
+            this.Btn_Start = new System.Windows.Forms.Button();
+            this.LbPing = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Txb_IP
+            // 
+            this.Txb_IP.Location = new System.Drawing.Point(12, 27);
+            this.Txb_IP.Name = "Txb_IP";
+            this.Txb_IP.Size = new System.Drawing.Size(100, 20);
+            this.Txb_IP.TabIndex = 0;
+            // 
+            // Txb_Port
+            // 
+            this.Txb_Port.Location = new System.Drawing.Point(118, 27);
+            this.Txb_Port.Name = "Txb_Port";
+            this.Txb_Port.Size = new System.Drawing.Size(38, 20);
+            this.Txb_Port.TabIndex = 1;
+            // 
+            // Btn_Start
+            // 
+            this.Btn_Start.Location = new System.Drawing.Point(226, 25);
+            this.Btn_Start.Name = "Btn_Start";
+            this.Btn_Start.Size = new System.Drawing.Size(45, 23);
+            this.Btn_Start.TabIndex = 2;
+            this.Btn_Start.Text = "Start";
+            this.Btn_Start.UseVisualStyleBackColor = true;
+            this.Btn_Start.Click += new System.EventHandler(this.Btn_Start_Click);
+            // 
+            // LbPing
+            // 
+            this.LbPing.AutoSize = true;
+            this.LbPing.Location = new System.Drawing.Point(162, 30);
+            this.LbPing.Name = "LbPing";
+            this.LbPing.Size = new System.Drawing.Size(13, 13);
+            this.LbPing.TabIndex = 3;
+            this.LbPing.Text = "_";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "IP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(115, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Port";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 53);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(259, 138);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 203);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LbPing);
+            this.Controls.Add(this.Btn_Start);
+            this.Controls.Add(this.Txb_Port);
+            this.Controls.Add(this.Txb_IP);
+            this.Name = "Main";
+            this.Text = "Ping Test";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Txb_IP;
+        private System.Windows.Forms.TextBox Txb_Port;
+        private System.Windows.Forms.Button Btn_Start;
+        private System.Windows.Forms.Label LbPing;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ PingTest/Main.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingTest
+{
+    public partial class Main : Form
+    {
+        string Server, User, Password;
+        string BinDir = AppDomain.CurrentDomain.BaseDirectory + @"Bin\";
+        public int ping = 0;
+        public string ms = " ms";
+        public int wakeywakey = 400;
+        public int sPort;
+        BackgroundWorker bw1 = new BackgroundWorker();
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        public void SetPing()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            bw1.WorkerSupportsCancellation = true;
+            bw1.WorkerReportsProgress = true;
+            bw1.DoWork += new DoWorkEventHandler(bw1_DoWork);
+            bw1.ProgressChanged += new ProgressChangedEventHandler(bw1_ProgressChanged);
+
+            //if (!string.IsNullOrWhiteSpace(Server))
+            //{
+            bw1.RunWorkerAsync();
+            //}
+        }
+
+        public void bw1_DoWork(object Sender, DoWorkEventArgs e)
+        {
+            // Set ping
+            //   AddTextLog("Getting Ping From Server...");
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(Txb_Port.Text))
+                {
+                    var sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    sock.Blocking = true;
+                    var stopwatch = new Stopwatch();
+                    stopwatch.Start();
+                    sock.Connect(Server, sPort);
+                    stopwatch.Stop();
+                    int ms = Convert.ToInt32(stopwatch.Elapsed.TotalMilliseconds);
+                    sock.Close();
+                    ping = ms;
+                }
+                else
+                {
+                    ping = Convert.ToInt32(new Ping().Send(Server).RoundtripTime.ToString());
+                }
+            }
+            catch { ping = 0; }
+            if (Regex.IsMatch(ping.ToString(), @"^\d+$") == true)
+            {
+                LbPing.Text = ping + ms;
+            }
+            else
+            {
+                LbPing.Text = "Offline";
+            }
+
+            BackgroundWorker worker = (BackgroundWorker)Sender;
+            while (!worker.CancellationPending)
+            {
+                // Loop Progress
+                worker.ReportProgress(0);
+                // Input Ping Into Label
+                Thread.Sleep(200);
+                try
+                {
+                    if (!string.IsNullOrWhiteSpace(Txb_Port.Text))
+                    {
+                        var sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                        sock.Blocking = true;
+                        var stopwatch = new Stopwatch();
+                        stopwatch.Start();
+                        sock.Connect(Server, sPort);
+                        stopwatch.Stop();
+                        int ms = Convert.ToInt32(stopwatch.Elapsed.TotalMilliseconds);
+                        sock.Close();
+                        ping = ms;
+                    }
+                    else
+                    {
+                        ping = Convert.ToInt32(new Ping().Send(Server).RoundtripTime.ToString());
+                    }
+                }
+                catch { ping = 0; }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (bw1 != null && bw1.IsBusy)
+            {
+                bw1.CancelAsync();
+            }
+        }
+
+        public void bw1_ProgressChanged(object Sender, ProgressChangedEventArgs e)
+        {
+            if (Regex.IsMatch(ping.ToString(), @"^\d+$") == true)
+            {
+                LbPing.Text = ping + ms;
+            }
+            else
+            {
+                LbPing.Text = "Offline";
+            }
+        }
+
+        private void Btn_Start_Click(object sender, EventArgs e)
+        {
+            Btn_Start.Text = "Start";
+            Int32.TryParse(Txb_Port.Text, out sPort);
+            Server = Txb_IP.Text;
+
+            if (bw1 != null && bw1.IsBusy)
+            {
+                bw1.CancelAsync();
+            }
+            else
+            {
+                SetPing();
+                Btn_Start.Text = "Stop";
+            }
+        }
+    }
+}
